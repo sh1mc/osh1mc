43259e6f7afd0e0cef6b26797e23bc8c845c9bc1
sh1mc <sh1@coins.tsukuba.ac.jp>
2021-08-16 01:11:19 +0900

    add timer interruption

--- src/lib.rs
+++ src/lib.rs
@@ -60,6 +60,8 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 pub fn init() {
     gdt::init();
     interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize() }
+    x86_64::instructions::interrupts::enable();
 }
 
 #[cfg(test)]
